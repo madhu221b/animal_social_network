a62052668efa72409a197adeee6dae063791d026
Papp Gergely <gergopool@gmail.com>
2023-06-10 17:31:21 +0200

    onclick event added

--- src/dashboard/main.py
+++ src/dashboard/main.py
@@ -11,7 +11,6 @@ import matplotlib
 
 matplotlib.use("Qt5Agg")
 
-
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
 
@@ -19,18 +18,43 @@ from netgraph import InteractiveGraph
 
 import networkx as nx
 
-graph_path = "/Users/madhurapawar/Documents/lab_assgs/mma/animalsocialnw_team7/datasets/vampirebats_carter_mouth_licking_attribute_new.graphml"
+graph_path = "datasets/vampirebats_carter_mouth_licking_attribute_new.graphml"
+
 
 class MplCanvas(FigureCanvasQTAgg):
+
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         super(MplCanvas, self).__init__(Figure(figsize=(width, height), dpi=dpi))
         self.setParent(parent)
         self.ax = self.figure.add_subplot(111)
+        self.mpl_connect('button_press_event', self.onclick)
         graph = nx.read_graphml(graph_path)
         self.plot_instance = InteractiveGraph(graph, ax=self.ax)
 
+    def onclick(self, event):
+        if event.xdata is not None:
+            node_name, node = self.get_closest_node(event.xdata, event.ydata)
+            print(node_name)
+        else:
+            print("Clicked somewhere else..")  # access at event.x, event.y
+
+    def get_closest_node(self, x, y):
+        # Loop over all nodes, select the one closest to click
+        closest_node = None
+        distance = 999999
+        cosest_node_name = None
+        for name in self.plot_instance.node_artists:
+            node = self.plot_instance.node_artists[name]
+            dist = ((x - node.xy[0])**2 + (y - node.xy[1])**2)**0.5
+            if dist < distance:
+                distance = dist
+                closest_node = node
+                cosest_node_name = name
+        return cosest_node_name, closest_node
+
 
 class MainWindow(QtWidgets.QMainWindow):
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
@@ -54,4 +78,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
