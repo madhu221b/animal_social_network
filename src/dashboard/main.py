a1e028e479a2c6042a67957a816888456fa79a9c
madhurapawaruva <117770386+madhurapawaruva@users.noreply.github.com>
2023-06-10 22:27:34 +0200

    Add files via upload

--- src/dashboard/main.py
+++ src/dashboard/main.py
@@ -1,80 +1,43 @@
-"""
-pip install pyqt5
-pip install netgraph
-"""
+from PyQt5 import QtCore, QtWidgets
+from canvas import Canvas
 
-import sys
+class ComboBox(QtWidgets.QComboBox):
+    popupAboutToBeShown = QtCore.pyqtSignal()
 
-from PyQt5 import QtWidgets
+    def showPopup(self):
+        self.popupAboutToBeShown.emit()
+        super(ComboBox, self).showPopup()
 
-import matplotlib
+class Window(QtWidgets.QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+        title = "Social Network Analysis of Animals"
+        self.setWindowTitle(title)
+        self.setGeometry(0, 0, 500, 300)
+        print("coming here")
+        self.combo = ComboBox(self)
+        self.combo.popupAboutToBeShown.connect(self.populateCombo)
+        self.combo.view().pressed.connect(self.handleItemPressed)
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.combo)
 
-matplotlib.use("Qt5Agg")
+    def populateCombo(self):
+        if not self.combo.count():
+            list_of_animals = ["Animal 1", "Animal 2", "Animal 3"]
+            self.combo.addItems(list_of_animals)
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
-from matplotlib.figure import Figure
+    def handleItemPressed(self, index):
+        item = self.combo.model().itemFromIndex(index)
+        print("Item Selected:", item.text())
+        self.w = Canvas(text=item.text())
+        self.w.show()
+        self.hide()
+      
 
-from netgraph import InteractiveGraph
+if __name__ == '__main__':
 
-import networkx as nx
-
-graph_path = "datasets/vampirebats_carter_mouth_licking_attribute_new.graphml"
-
-
-class MplCanvas(FigureCanvasQTAgg):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        super(MplCanvas, self).__init__(Figure(figsize=(width, height), dpi=dpi))
-        self.setParent(parent)
-        self.ax = self.figure.add_subplot(111)
-        self.mpl_connect('button_press_event', self.onclick)
-        graph = nx.read_graphml(graph_path)
-        self.plot_instance = InteractiveGraph(graph, ax=self.ax)
-
-    def onclick(self, event):
-        if event.xdata is not None:
-            node_name, node = self.get_closest_node(event.xdata, event.ydata)
-            print(node_name)
-        else:
-            print("Clicked somewhere else..")  # access at event.x, event.y
-
-    def get_closest_node(self, x, y):
-        # Loop over all nodes, select the one closest to click
-        closest_node = None
-        distance = 999999
-        cosest_node_name = None
-        for name in self.plot_instance.node_artists:
-            node = self.plot_instance.node_artists[name]
-            dist = ((x - node.xy[0])**2 + (y - node.xy[1])**2)**0.5
-            if dist < distance:
-                distance = dist
-                closest_node = node
-                cosest_node_name = name
-        return cosest_node_name, closest_node
-
-
-class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-
-        self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
-        self.toolbar = NavigationToolbar2QT(self.canvas, self)
-
-        widget = QtWidgets.QWidget()
-        self.setCentralWidget(widget)
-
-        layout = QtWidgets.QVBoxLayout(widget)
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-
-
-def main():
+    import sys
     app = QtWidgets.QApplication(sys.argv)
-    w = MainWindow()
-    w.show()
-    app.exec_()
-
-
-if __name__ == "__main__":
-    main()
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
