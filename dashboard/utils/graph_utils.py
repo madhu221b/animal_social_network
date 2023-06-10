7e14ad3b3dbed47b26398549261792a85badf4fe
madhurapawaruva <117770386+madhurapawaruva@users.noreply.github.com>
2023-06-10 17:47:54 +0200

    Click + Dashboard interface

---
+++ dashboard/utils/graph_utils.py
@@ -0,0 +1,32 @@
+import networkx as nx
+import numpy as np
+
+from matplotlib import cm, colors
+import matplotlib.pyplot as plt
+
+shades = plt.get_cmap('Pastel1')
+
+
+random_state = np.random.RandomState(42)
+
+def read_graph(path, is_add_new_nodes=False):
+    g = nx.read_graphml(path)
+
+    edge_color, node_color = dict(), dict()
+    if is_add_new_nodes:
+         print("Implementation to Build")
+
+    
+    for _, edge in enumerate(g.edges):
+        edge_color[edge] = 'tab:gray' 
+    
+    minval = min([degree for _, degree in g.degree()])
+    maxval = max([degree for _, degree in g.degree()])
+    norm = colors.Normalize(vmin=minval, vmax=maxval, clip=True)
+    mapper = cm.ScalarMappable(norm=norm, cmap=shades)
+    
+    for node, degree in g.degree():
+        node_color[node] =  mapper.to_rgba(degree)
+    # node_color = np.asarray([degrees[n] for n in nodes])
+   
+    return g, edge_color, node_color
