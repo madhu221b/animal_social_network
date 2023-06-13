7cb3e3fba7329b020a7757233b29b4eaab6e0537
Papp Gergely <gergopool@gmail.com>
2023-06-13 08:24:24 +0200

    icons added

--- src/dashboard/landing_page.py
+++ src/dashboard/landing_page.py
@@ -20,7 +20,7 @@ class LandingPage(QtWidgets.QWidget):
 
     WINDOW_TITLE = "Social Network Analysis of Animals"
     WINDOW_WIDTH = 500
-    WINDOW_HEIGHT = 300
+    WINDOW_HEIGHT = 100
     USER_OPTIONS = list(GRAPHS.keys())
 
     def __init__(self):
@@ -29,42 +29,62 @@ class LandingPage(QtWidgets.QWidget):
         # Set default layout
         self.setWindowTitle(self.WINDOW_TITLE)
         self.setGeometry(0, 0, self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
-        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout = QtWidgets.QHBoxLayout(
+            self)  # Change to QHBoxLayout to place elements side by side
 
         # Add dropdown list to the window
         self._create_dropdown_list()
+
+        # Add select button to the window
+        self._create_select_button()
+
+        # Center the window on the screen
+        self._center_window()
+
+    def _center_window(self):
+        """Center the window on the screen"""
+        screen_geometry = QtWidgets.QApplication.desktop().screenGeometry()
+        x = (screen_geometry.width() - self.WINDOW_WIDTH) // 2
+        y = (screen_geometry.height() - self.WINDOW_HEIGHT) // 2
+        self.move(x, y)
 
     def _create_dropdown_list(self):
         """Create a clickable dropdown list to this window"""
         dropdown_list = DropDownListBox(self)
 
         # Define all choices the users can choose from
-        self._add_default_choices_to_dropdown(dropdown_list)
-
-        # Set click events
-        dropdown_list.view().pressed.connect(self._dropdown_on_click)
+        dropdown_list.addItems(self.USER_OPTIONS)
+        dropdown_list.setCurrentIndex(0)  # Select the first item by default
 
         # Add widget to the layout
         self.layout.addWidget(dropdown_list)
         self.dropdown_list = dropdown_list
 
-    def _add_default_choices_to_dropdown(self, drop_down_list: DropDownListBox):
-        """Creating a list of choices the users can choose from"""
+    def _create_select_button(self):
+        """Create a select button to this window"""
+        select_button = QtWidgets.QPushButton("Select", self)
 
-        def _populate_combo():
-            if not drop_down_list.count():
-                drop_down_list.addItems(self.USER_OPTIONS)
+        # Set click event
+        select_button.clicked.connect(self._select_button_on_click)
 
-        drop_down_list.popup_dropdown_window.connect(_populate_combo)
+        # Calculate the preferred size of the button
+        size_hint = select_button.sizeHint()
 
-    def _dropdown_on_click(self, index: int):
-        """When user selects an item by clicking on it, close this 'menu' window
-           and open the desired dashboard
+        # Set the fixed width based on the preferred size
+        select_button.setFixedWidth(size_hint.width())
+
+        # Add widget to the layout
+        self.layout.addWidget(select_button)
+
+    def _select_button_on_click(self):
+        """
+        When user clicks the select button, close this 'menu' window
+        and open the desired dashboard
         """
 
         # Select item
-        item = self.dropdown_list.model().itemFromIndex(index)
-        print("Item Selected:", item.text())
+        item = self.dropdown_list.currentText()
+        print("Item Selected:", item)
 
         # In case we already opened, close the previous one
         if hasattr(self, "main_window"):
@@ -72,9 +92,14 @@ class LandingPage(QtWidgets.QWidget):
             self.main_window.destroy()
 
         # Create new window and hide this one
-        self.main_window = MainCanvas(text=item.text())
+        self.main_window = MainCanvas(text=item)
         self.main_window.show()
         self.hide()
+
+    def _simulate_select_first_item(self):
+        """Simulate selecting the first item for development purposes"""
+        self.dropdown_list.setCurrentIndex(0)
+        self._select_button_on_click()
 
 
 if __name__ == '__main__':
