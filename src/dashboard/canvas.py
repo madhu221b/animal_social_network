b184b8b577bcf9a867794fd56100dde40b77ed81
Papp Gergely <gergopool@gmail.com>
2023-06-13 11:38:05 +0200

    attributes layout change

--- src/dashboard/canvas.py
+++ src/dashboard/canvas.py
@@ -1,7 +1,7 @@
 import os
 
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QTabWidget, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QToolBar, QAction, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QTabWidget, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QToolBar, QAction, QMessageBox, QTableWidget, QTableWidgetItem
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
@@ -19,6 +19,7 @@ from src.dashboard.slider import BSlider, CSlider
 from src.dashboard.loaders.bat_loader import load_dataset
 
 from src.dashboard.graph_analytics import GraphAnalytics
+from src.dashboard.utils.common import swap_dict_keys
 
 DATASETS_PATH = os.getcwd().split("src")[0] + "/datasets"
 
@@ -151,36 +152,68 @@ class NodeInfoPage(QWidget):
     def __init__(self, features, metrics):
         super(NodeInfoPage, self).__init__()
 
+        self.LEFT_WIDTH = 200
+        self.RIGHT_WIDTH = 200
+        self.CELL_HEIGHT = 30
+        self.FEATURES = ['population', 'Group', 'sex']
+        self.METRICS = None  # None == select all
+
         self.features = features
-        self.metrics = metrics
+        self.metrics = swap_dict_keys(metrics)
 
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
+        self.feature_title_label = QLabel("Features")
+        self.feature_title_label.setStyleSheet("font-weight: bold; font-size: 16px;")
+        self.layout.addWidget(self.feature_title_label)
+
+        self.feature_table = self._create_table(self.features, self.LEFT_WIDTH, self.FEATURES)
+        self.layout.addWidget(self.feature_table)
+
+        self.metric_title_label = QLabel("Metrics")
+        self.metric_title_label.setStyleSheet("font-weight: bold; font-size: 16px;")
+        self.layout.addWidget(self.metric_title_label)
+
+        self.metric_table = self._create_table(metrics, self.RIGHT_WIDTH, self.METRICS)
+        self.layout.addWidget(self.metric_table)
+
+        self.layout.addStretch(1)
+
+    def _create_table(self, data, width, columns=None):
+        table = QTableWidget()
+        table.setFixedWidth(width)
+        table.setColumnCount(2)
+        table.horizontalHeader().hide()
+        table.verticalHeader().hide()
+        table.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        table.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        # Populate the table with titles and dummy values
+        columns = columns if columns is not None else list(data.keys())
+        for key in columns:
+            row = table.rowCount()
+            table.insertRow(row)
+            item = QTableWidgetItem(f"{key}")
+            item.setFont(QFont('Arial', 10, QFont.Bold))
+            table.setItem(row, 0, item)
+            table.setItem(row, 1, QTableWidgetItem(""))
+
+        table.setFixedHeight(self.CELL_HEIGHT * table.rowCount())
+
+        return table
+
     def update(self, node_name):
-        while self.layout.count():
-            item = self.layout.takeAt(0)
-            widget = item.widget()
-            if widget is not None:
-                widget.deleteLater()
+        if node_name:
+            self._update_table(self.feature_table, self.features[node_name])
+            self._update_table(self.metric_table, self.metrics[node_name])
 
-        if not node_name:
-            return
-
-        title_label = QLabel('Features')
-        title_label.setStyleSheet("font-weight: bold; font-size: 18px;")
-        self.layout.addWidget(title_label)
-        for feat in self.features[node_name]:
-            if feat == 'population' or feat == 'Group' or feat == 'sex':
-                feature_label = QLabel(f"{feat}: {self.features[node_name][feat]}")
-                self.layout.addWidget(feature_label)
-
-        title_label = QLabel('Metrics')
-        title_label.setStyleSheet("font-weight: bold; font-size: 18px;")
-        self.layout.addWidget(title_label)
-        for key, value in self.metrics.items():
-            metric_label = QLabel(f"{key}: {round(value[node_name], 2)}")
-            self.layout.addWidget(metric_label)
+    def _update_table(self, table, data):
+        for row in range(table.rowCount()):
+            key_item = table.item(row, 0)
+            value = data.get(key_item.text(), "")
+            value = f"{value:.2f}" if isinstance(value, float) else value
+            table.setItem(row, 1, QTableWidgetItem(str(value)))
 
 
 class MainCanvas(QMainWindow):
@@ -188,8 +221,8 @@ class MainCanvas(QMainWindow):
     This is the main window, with tabs and 1 canvas (dashboard/page) for each tab.
     """
 
-    WINDOW_HEIGHT = 700
-    WINDOW_WIDTH = 700
+    WINDOW_HEIGHT = 600
+    WINDOW_WIDTH = 1000
 
     def __init__(self, text, *args, **kwargs):
         super(MainCanvas, self).__init__(*args, **kwargs)
