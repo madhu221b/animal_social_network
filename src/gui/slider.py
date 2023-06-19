3a3a3e30e3c191f38a25200db0a94a5d1159fb23
JonasElburgUVA <114097649+JonasElburgUVA@users.noreply.github.com>
2023-06-19 13:10:20 +0200

    Update slider.py

--- src/gui/slider.py
+++ src/gui/slider.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QSlider, QLabel, QFormLayout
-from PyQt5.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QWidget, QSlider, QLabel, QFormLayout
+from PyQt6.QtCore import Qt
 
 
 class BSlider(QWidget):
