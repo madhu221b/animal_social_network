af5f91521728731c348f4b26e89dfd6b6f1d116b
madhurapawaruva <madhura.pawar@student.uva.nl>
2023-06-20 15:47:57 +0200

    dynamic tab updation.

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,8 @@
 import os
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QMainWindow, QTabWidget
+from PyQt6.QtWidgets import QMainWindow, QTabWidget, QWidget
 from PyQt6.QtGui import QGuiApplication
+from PyQt6.QtCore import pyqtSignal
 
 from src.gui.graph_analytics import GraphAnalytics
 from .social_graph import GraphPage
@@ -13,6 +14,7 @@ class MainWindow(QMainWindow):
     This is the main window, with tabs and 1 canvas (dashboard/page) for each tab.
     """
 
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle(PageState.title)
@@ -23,15 +25,28 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
         # Add pages
-        tabs = QTabWidget()
+        self.tabs = QTabWidget()
         self.graph_page = GraphPage(self)
-        self.graph_analytics = GraphAnalytics(self)
-        tabs.addTab(self.graph_page, "Social Graph")
-        tabs.addTab(self.graph_analytics, "Graph Analytics")
-        self.layout.addWidget(tabs)
+   
+        self.tabs.addTab(self.graph_page, "Social Graph")
+        self.tabs.addTab(QWidget(), "Graph Analytics")
+      
+        self.tabs.tabBarClicked.connect(self.updateTab)
+        self.layout.addWidget(self.tabs)
 
         # Center the window on the screen
         self._center_window()
+    
+    def updateTab(self, tabIndex):
+        pass
+        if tabIndex == 1:
+            self.updateGraphTab()
+    
+    def updateGraphTab(self):
+        self.tabs.removeTab(1)
+        self.graph_analytics = GraphAnalytics(self)
+        self.tabs.addTab(self.graph_analytics, "Graph Analytics") # <--- add tab
+
 
     def _center_window(self):
         """Center the window on the screen"""
