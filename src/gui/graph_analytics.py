bb046df30ec010266281fede3b405f8843636bd5
JonasElburgUVA <114097649+JonasElburgUVA@users.noreply.github.com>
2023-06-20 10:16:53 +0200

    Added attribute distribution plot

--- src/gui/graph_analytics.py
+++ src/gui/graph_analytics.py
@@ -11,62 +11,63 @@ matplotlib.use("Qt5Agg")
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
-class GraphAnalytics(FigureCanvasQTAgg):
+class GraphAnalytics(QWidget):
     """
-    Graph analytics page, contains metrics and visualisations
+    Graph analytics page, contains metrics and visualizations
     """
 
-    def __init__(self, parent, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        super().__init__(fig)
+    def __init__(self, parent):
+        super().__init__(parent)
         self.parent = parent
         self.setup_ui()
 
     def setup_ui(self):
-        main_widget = QWidget(self)
-        self.setContentsMargins(0, 0, 0, 0)
-        main_layout = QVBoxLayout(main_widget)
+        main_layout = QVBoxLayout(self)
 
-        # Add  plots
-        plot1 = self.create_plot()
-        main_layout.addWidget(plot1)
+        # Add GraphAnalytics canvas
+        graph_canvas = self.sampleplot()#GraphCanvas(self)
+        main_layout.addWidget(graph_canvas)
 
-        # self.setParent(main_widget)
-        # self.setFocusPolicy(QtCore.Qt.FocusPolicy.StrongFocus)
+        # Add attribute distribution plot
+        attribute_distribution_plot = self.attribute_distribution_plot()
+        main_layout.addWidget(attribute_distribution_plot)
 
-    def create_plot(self):
-        fig = self.figure
+        # Add adjacency matrix label
+        adj_matrix_label = QLabel("Adjacency Matrix", self)
+        main_layout.addWidget(adj_matrix_label)
+
+    def sampleplot(self):
+        fig = Figure(figsize=(5, 4), dpi=100)
         ax = fig.add_subplot(111)
+        ax.set_title('title')
         ax.plot([1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
 
         return FigureCanvasQTAgg(fig)
 
+    def attribute_distribution_plot(self):
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.set_title('Attribute Distribution')
+        node_features = self.parent.graph_page.graph_page.features
+        attribute_names = set([key for _, value in node_features.items() for key, _ in value.items()])
 
-
-        # self.figure = Figure(figsize=(width, height), dpi=dpi)
-
-        # self.parent = parent
-        # self.setParent(parent)
-        # self.ax = self.figure.add_subplot(211)
-        # self.node_features = self.parent.graph_page.graph_page.features
-
-        # attribute_names = set([key for _, value in self.node_features.items() for key, _ in value.items()])
-
-        # attribute_counts = {}
-        # for att in attribute_names:
-        #     attribute_counts[att] = []
+        attribute_counts = {}
+        for att in attribute_names:
+            attribute_counts[att] = []
         
-        # for features in self.node_features.values():
-        #     for att in attribute_names:
-        #         if att in features.keys():
-        #             attribute_counts[att].append(features[att])
+        for features in node_features.values():
+            for att in attribute_names:
+                if att in features.keys():
+                    attribute_counts[att].append(features[att])
 
-        # for attribute, counts in attribute_counts.items():
-        #     element_counts = Counter(counts)
-        #     values = list(element_counts.keys())
-        #     count = list(element_counts.values())
+        for attribute, counts in attribute_counts.items():
+            element_counts = Counter(counts)
+            values = list(element_counts.keys())
+            count = list(element_counts.values())
 
-        #     self.ax.barh(attribute, count[0], label=values[0], color=shades(0))
-        #     self.ax.legend(loc='upper center', ncol=len(values), bbox_to_anchor=(0.5, -0.15))
-        #     for i in range(1, len(values)):
-        #         self.ax.barh(attribute, count[i], left=sum(count[:i]), label=values[i], color=shades(i))
+            ax.barh(attribute, count[0], label=values[0], color=shades(0))
+            ax.legend(loc='upper center', ncol=len(values), bbox_to_anchor=(0.5, -0.15))
+            for i in range(1, len(values)):
+                ax.barh(attribute, count[i], left=sum(count[:i]), label=values[i], color=shades(i))
+        
+        return FigureCanvasQTAgg(fig)
