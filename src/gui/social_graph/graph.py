3abde0a07f72a9723a4995c8613e2ba87f591ada
Papp Gergely <gergopool@gmail.com>
2023-06-23 14:05:39 +0200

    prediction still fails

--- src/gui/social_graph/graph.py
+++ src/gui/social_graph/graph.py
@@ -107,6 +107,9 @@ class GraphCanvas(FigureCanvasQTAgg):
     def refresh(self):
         self.ax.cla()  # Clears the existing plot
         pos = nx.spring_layout(self.graph.graph, k=math.sqrt(1 / self.graph.graph.order()))
+        import numpy as np
+        if np.isnan(list(pos.values())[0][0]):
+            pos = nx.circular_layout(self.graph.graph)
 
         if self.graph.node_layout is not None:
             for key, value in pos.items():
